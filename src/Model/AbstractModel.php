64c466c83ad4ca64d755cc74ab0c37dde695812b
Carlos Herrera <18551113+caherrera@users.noreply.github.com>
2021-08-25 09:15:27 -0400

    magic methods

--- src/Model/AbstractModel.php
+++ src/Model/AbstractModel.php
@@ -44,7 +44,7 @@ abstract class AbstractModel
     }
 
     /**
-     * @param Client|null $client
+     * @param  Client|null  $client
      *
      * @return $this
      */
@@ -52,35 +52,6 @@ abstract class AbstractModel
     {
         if (null !== $client) {
             $this->client = $client;
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param array<string,mixed> $data
-     *
-     * @return $this
-     */
-    protected function hydrate(array $data = [])
-    {
-        foreach ($data as $field => $value) {
-            $this->setData($field, $value);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param string $field
-     * @param mixed  $value
-     *
-     * @return $this
-     */
-    protected function setData(string $field, $value)
-    {
-        if (\in_array($field, static::$properties, true)) {
-            $this->data[$field] = $value;
         }
 
         return $this;
@@ -95,28 +66,30 @@ abstract class AbstractModel
     }
 
     /**
-     * @param string $property
-     * @param mixed  $value
+     * @param  string  $field
+     * @param  mixed  $value
      *
-     * @throws RuntimeException
-     *
-     * @return void
+     * @return $this
      */
-    public function __set(string $property, $value): void
+    protected function setData(string $field, $value)
     {
-        throw new RuntimeException('Model properties are immutable');
+        if (\in_array($field, static::$properties, true)) {
+            $this->data[$field] = $value;
+        }
+
+        return $this;
     }
 
     /**
-     * @param string $property
-     *
-     * @throws RuntimeException
+     * @param  string  $property
      *
      * @return mixed
+     * @throws RuntimeException
+     *
      */
     public function __get(string $property)
     {
-        if (!\in_array($property, static::$properties, true)) {
+        if ( ! \in_array($property, static::$properties, true)) {
             throw new RuntimeException(\sprintf('Property "%s" does not exist for %s object', $property, static::class));
         }
 
@@ -128,12 +101,49 @@ abstract class AbstractModel
     }
 
     /**
-     * @param string $property
+     * @param  string  $property
+     * @param  mixed  $value
+     *
+     * @return void
+     * @throws RuntimeException
+     *
+     */
+    public function __set(string $property, $value): void
+    {
+        throw new RuntimeException('Model properties are immutable');
+    }
+
+    /**
+     * @param  string  $property
      *
      * @return bool
      */
     public function __isset(string $property)
     {
         return isset($this->data[$property]);
+    }
+
+    public function __serialize()
+    {
+        return $this->data;
+    }
+
+    public function __unserialize($data)
+    {
+        $this->hydrate($data);
+    }
+
+    /**
+     * @param  array<string,mixed>  $data
+     *
+     * @return $this
+     */
+    protected function hydrate(array $data = [])
+    {
+        foreach ($data as $field => $value) {
+            $this->setData($field, $value);
+        }
+
+        return $this;
     }
 }
